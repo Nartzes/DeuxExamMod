fc2b7274fe2e942342f882ae24043eda55943ed3
Khai Ha <Khaiha265@gmail.com>
2024-04-18 18:33:43 -0700

    Add project files.

---
+++ Items/FeelGoodJuice.cs
@@ -0,0 +1,30 @@
+﻿using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DeuxExamMod.Items
+{
+    public class FeelGoodJuice : ModItem
+    {
+        public override void SetDefaults()
+        {
+            Item.potion = true;
+            Item.maxStack = 99;
+            Item.value = 10020;
+            Item.rare = 2;
+            Item.useAnimation = 30;
+            Item.useTime = 30;
+            Item.useStyle = 9;
+            Item.UseSound = SoundID.Item3;
+            Item.consumable = true;
+        }
+
+        public override bool? UseItem(Player player)
+        {
+            player.AddBuff(BuffID.Ironskin, 7200);
+            player.AddBuff(BuffID.Wrath, 7200);
+            player.AddBuff(BuffID.Slow, 2000);
+            return true;
+        }
+    }
+}
---
+++ Items/FratPad.cs
@@ -0,0 +1,27 @@
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace DeuxExamMod.Items
+{
+	public class FratPad : ModItem
+	{
+
+		public override void SetDefaults()
+		{
+			Item.damage = 100;
+			Item.DamageType = DamageClass.Melee;
+			Item.crit = 16;
+			Item.width = 40;
+			Item.height = 40;
+			Item.useTime = 30;
+			Item.useAnimation = 20;
+			Item.useStyle = 1;
+			Item.knockBack = 8;
+			Item.value = 10000;
+			Item.rare = 2;
+			Item.UseSound = SoundID.Item1;
+			Item.autoReuse = true;
+		}
+	}
+}
