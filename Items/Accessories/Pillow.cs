fc07306b5dd195ae62bd211706b1b24904abb0fe
jonathan wilkerson <jonathan.wilkerson@student.centralia.edu>
2024-04-29 23:29:50 -0700

    pillow assories

---
+++ Items/Accessories/Pillow.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+using Humanizer;
+
+
+namespace DeuxExamMod.Items.Accessories
+{
+    public class Pillow : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Pilow ");
+            Tooltip.SetDefault("you look sleep i can fix thaaat");
+        }
+        public override void SetDefaults() 
+        {
+            Item.width = 20;
+            Item.height = 20;
+
+            Item.accessory = true;
+        }
+        public override void UpdateAccesory(Player player,bool hideVisual)
+        {
+            player.GetDamage(DamageClass.Generic) += 0.25f;
+        }
+    }
+}
